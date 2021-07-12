afe755218dd15dbdacb4682bde6657abe8d86b22
ArthurReisRatti <85950635+ArthurReisRatti@users.noreply.github.com>
2021-07-12 11:58:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 (async () => {
-    const url = "mongodb://localhost:27017";
+    const url = "mongodb+srv://admin:Gzzzy7DfT8yXDetS@cluster1.kwnh5.mongodb.net/ocean_db?retryWrites=true&w=majority";
     const dbName = "ocean_bancodados_09_07_2021";
 
     console.info("Conectando ao banco de dados...");
 
-    //const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
     console.info("MongoDB conectado com sucesso!");
 
-    //const db = client.db(dbName);
+    const db = client.db(dbName);
 
     const app = express();
 
@@ -43,7 +43,7 @@ const { MongoClient, ObjectId } = require("mongodb");
     const lista = ["Senhor dos Anéis", "Harry Potter"];
     //              0                   1
 
-    //const filmes = db.collection("filmes");
+    const filmes = db.collection("filmes");
     const filmes = undefined;
 
     // [GET] - Read All
